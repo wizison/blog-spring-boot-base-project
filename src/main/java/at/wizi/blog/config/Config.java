35720017284d51b0eeaa1fa46715199836be5efb
wizi <witzelsperger@gmail.com>
2018-02-18 17:09:44 +0100

    init gradle and spring boot project

---
+++ src/main/java/at/wizi/blog/config/Config.java
@@ -0,0 +1,9 @@
+package at.wizi.blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"at.wizi.blog"})
+public class Config {
+}
